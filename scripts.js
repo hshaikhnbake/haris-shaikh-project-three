304eb22a9e6f381f0516386db00cd4230fdc7d4e
Haris Shaikh <shaikh.m.haris@gmail.com>
2020-09-15 03:25:29 -0400

    2nd last commit until live. Site responsive. Ready to be executed. Adding comments in code

--- scripts.js
+++ scripts.js
@@ -1,19 +1,12 @@
+
+//Baseline App Variables
 const cardApp = {}
 cardApp.card = $('.memoryCard')
 cardApp.cardMatchContainer = [];
 
+//App Features
 
-cardApp.cardMatch = function () {
-    if (cardApp.cardMatchContainer.length === 2) {
-        if (cardApp.cardMatchContainer[0].data('type') === cardApp.cardMatchContainer[1].data('type')) {
-            cardApp.match();
-
-        } else {
-            cardApp.unmatch();
-        }
-    }
-}
-
+//Audio Components
 cardApp.playMainAudio = function () {
     $('.soundOn').on('click', soundOn);
 
@@ -30,6 +23,7 @@ cardApp.pauseMainAudio = function () {
     }
 }
 
+//Smooth Scroll Feature
 cardApp.scroll = function () {
     $(".startGame").click(function () {
         $('html, body').animate({
@@ -38,10 +32,22 @@ cardApp.scroll = function () {
     });
 }
 
+// Card Match If Function
+cardApp.cardMatch = function () {
+    if (cardApp.cardMatchContainer.length === 2) {
+        if (cardApp.cardMatchContainer[0].data('type') === cardApp.cardMatchContainer[1].data('type')) {
+            cardApp.match();
 
+        } else {
+            cardApp.unmatch();
+        }
+    }
+}
+
+//Main Card Match Function
 cardApp.cardClick = function () {
     //Event Listener on any cardApp.card to execute function of flipCard
-    
+
     cardApp.card.on('click', flipCard);
     //flipCard function toggles '.flip' class
     function flipCard() {
@@ -54,9 +60,11 @@ cardApp.cardClick = function () {
         }
 
 
-        // if length of cards is equal to 2, check to see if the two cards match. If they do, run a match function. If they don't run a unmatched function. 
+        // if length of cards is equal to 2, check to see if the two cards match based on data attribute of "type". If they do, run a match function. If they don't run a unmatched function. 
         cardApp.cardMatch();
 
+        //lockboard prevents the game to crash. Additional clicks during the unmatch function are temporarily paused. Matched cards will not be allowed to flip back.
+        
         cardApp.lockBoard = function () {
             cardApp.card.off('click', flipCard)
         }
